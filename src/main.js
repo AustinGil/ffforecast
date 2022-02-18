78a59cdf4607a2bb935982c88335525f5ca801b0
Austin Gil <hey@austingil.com>
2022-02-17 16:01:52 -0800

    Addign cards and layout

--- src/main.js
+++ src/main.js
@@ -46,13 +46,33 @@ export async function responseProvider(request) {
         tr:nth-child(even) {
           background-color: ButtonFace;
         }
-        tr:not(:last-child) th,
-        tr:not(:last-child) td {
+        tr:not(:last-child) :is(th, td) {
           border-bottom: 1px solid;
         }
-        ${[4, 8, 16, 32]
+        .relative {
+          position: relative;
+        }
+        .flex {
+          display: flex;
+        }
+        .align-center {
+          align-items: center;
+        }
+        .justify-between {
+          justify-content: space-between;
+        }
+        .min-w-120 {
+          min-inline-size: ${120 / 16}rem;
+        }
+        .o-auto {
+          overflow: auto;
+        }
+        ${[0, 4, 8, 12, 16, 32]
           .map((space) => {
             return `
+          .gap-${space} {
+            gap: ${space / 16}rem;
+          }
           .m-${space} {
             margin: ${space / 16}rem;
           }
@@ -64,11 +84,29 @@ export async function responseProvider(request) {
           }`;
           })
           .join('')}
+        .b-1 {
+          border: 1px solid;
+        }
+        .card {
+          position: relative;
+          border: 1px solid;
+          border-radius: 0.5rem;
+          padding: .75rem;
+          overflow: hidden;
+        }
+        .card:before {
+          content: '';
+          position: absolute;
+          z-index: -1;
+          inset: 0;
+          opacity: 0.1;
+          background-color: CanvasText;
+        }
       </style>
     </head>
   
     <body>
-      <main class="p-16">
+      <main class="p-8">
         <h1 class="mb-8">Friendly, Fast Forecast</h1>
 
         ${
@@ -76,31 +114,26 @@ export async function responseProvider(request) {
           !weatherData
             ? `<p>No weather data for: ${city}</p>`
             : `<h2 class="mb-32">${city} ${formatDate(
-                weatherData.current.dt * 1000
+              weatherData.current.dt * 1000,
+              { dateStyle: 'short', timeStyle: 'short' }
               )}</h2>
             
-            <section class="mb-32">
-              <h3 class="mb-8">Current</h3>
-            <p>
+            <section class="card mb-32">
+              <h3 class="h6 mb-8">Current</h3>
+            <p class="flex align-center justify-between m-0">
+              <span>
+                <span class="h1">${weatherData.current.temp}℉</span>
+                <br>
+                <span>${weatherData.current.weather[0].description}</span>
+              </span>
               <img src="http://openweathermap.org/img/wn/${
                 weatherData.current.weather[0].icon
-              }@2x.png" alt="${weatherData.current.weather[0].description}"/>
-              ${weatherData.current.weather[0].description} | ${
-                weatherData.current.temp
-              }℉
+    }@2x.png" alt="${weatherData.current.weather[0].description}"/>
             </p>
             <table>
               <tr>
                 <th align="left">Feels Like</th>
                 <td>${weatherData.current.feels_like}℉</td>
-              </tr>
-              <tr>
-                <th align="left">Min</th>
-                <td>${weatherData.daily[0].temp.min}℉</td>
-              </tr>
-              <tr>
-                <th align="left">Max</th>
-                <td>${weatherData.daily[0].temp.max}℉</td>
               </tr>
               <tr>
                 <th align="left">Wind Speed</th>
@@ -114,23 +147,34 @@ export async function responseProvider(request) {
                 <th align="left">Cloudiness</th>
                 <td>${weatherData.current.clouds}%</td>
               </tr>
-              <tr>
-                <th align="left">Sunrise</th>
-                <td>${formatDate(weatherData.current.sunrise * 1000, {
-                  timeStyle: 'short',
-                })}</td>
-              </tr>
-              <tr>
-                <th align="left">Sunset</th>
-                <td>${formatDate(weatherData.current.sunset * 1000, {
-                  timeStyle: 'short',
-                })}</td>
-              </tr>
             </table>
           </section>
 
-          <section class="mb-32">
-            <h3 class="mb-8">Hourly</h3>
+          <section class="card mb-32">
+            <h3 class="h6 mb-8">Hourly</h3>
+            <div class="flex o-auto">
+              ${weatherData.hourly
+      .map((hour) => {
+        return `
+              <div class="min-w-120">
+                <small>
+                ${formatDate(hour.dt * 1000, {
+          dateStyle: 'short',
+        })}
+                </small>
+                <br>
+                <span class="h4"><b>${formatDate(hour.dt * 1000, {
+          timeStyle: 'short',
+        })}</b></span>
+                <br>
+                <img src="http://openweathermap.org/img/wn/${hour.weather[0].icon
+          }@2x.png" alt="${hour.weather[0].description}" width="64"/>
+                <br>
+                <span>${hour.temp}℉</span>
+              </div>`;
+      })
+      .join('')}
+            </div>
             <div style="max-block-size: 50vh; overflow: auto;">
               <table>
               ${weatherData.hourly
@@ -150,7 +194,7 @@ export async function responseProvider(request) {
             </div>
           </section>
           
-          <section class="mb-32">
+          <section class="card mb-32">
             <h3 class="mb-8">Daily</h3>
             <div style="max-block-size: 50vh; overflow: auto;">
               <table>
